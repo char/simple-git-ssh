ab9a758caae58d2171cb4d943cc98e033c6b3814
Charlotte Som <charlotte@som.codes>
2026-04-19 13:46:24 +0100

    add a nix package
    the symlink is a hack but i didnt want to write like a fetchZigPackage guy so

--- flake.nix
+++ flake.nix
@@ -10,6 +10,28 @@
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in {
+      packages = forAllSystems (pkgs: let
+        shlex = pkgs.fetchFromGitHub {
+          owner = "char";
+          repo = "shlex";
+          rev = "293b7c4bdc3b6c9a3195d226f0d8e92bf4516f96";
+          hash = "sha256-aL5AJYVjnmvu8BJMD3f1z99e4MGJgNkUrOidWhr27NE=";
+        };
+      in {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "simple-git-ssh";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.zig ];
+
+          postPatch = ''
+            mkdir -p zig-pkg
+            ln -s ${shlex} zig-pkg/shlex-0.1.1-O1DEI5oDAQARcXkYasXdAAsR0riDfb-1_riTtx0vemxN
+          '';
+        };
+      });
+
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           nativeBuildInputs = [
